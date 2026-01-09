29a5e9b8bd817bd9779a7dd3cd082c4fb5018634
SirioRedGiant <Oiris1992@gmail.com>
2026-01-09 18:16:02 +0100

    aggiunto commento alla funzione

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -5,7 +5,13 @@ const character = "A";
 // Dichiara la funzione qui.
 
 //! FUNZIONE TRADIZIONALE
-
+/**
+ *todo      FUNZIONE CHE DATO UN ARRAY E UNA LETTERA RESTITUISCE UN ARRAY CONTENENTE STRINGHE CHE INIZIANO CON LO STESSO CARATTERE
+            Nel ciclo controllo che il carattere dato "firstCharacter" sia identico al carattere della stringa fornito. Se è identico lo aggiungo alla nuova lista "initial"
+ *          @param {string[]} list 
+ *          @param {string} firstCharacter 
+ *          @returns {string[]}
+ */
 function wordInitialComparator(list, firstCharacter) {
   const initial = [];
   for (let i = 0; i < list.length; i++) {
