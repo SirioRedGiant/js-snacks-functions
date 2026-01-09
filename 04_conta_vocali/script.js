694463db8b5203a50d99bf569a3b64e3ad40490f
SirioRedGiant <Oiris1992@gmail.com>
2026-01-09 19:28:11 +0100

    Arrow Function version

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -10,9 +10,9 @@ const typeOfVowels = ["a", "e", "i", "o", "u"];
  *todo      Funzione che data una stringa ed un array di vocali ritorna il numero di vocali presenti all'interno della stringa fornitogli
             definita la variabile con let perchè deve modificarsi ad ogni ciclo aumentando
             Utilizzo il doppio ciclo prendendo col ciclo esterno una lettera e con quello interno controllo se quella lettera coincide con una delle vocali
- * @param {string} term 
- * @param {string[]} vocoidList 
- * @returns 
+ *          @param {string} term 
+ *          @param {string[]} vocoidList 
+ *          @returns 
  */
 function vowelsCounter(term, vocoidList) {
   let vocoidCounter = 0;
@@ -26,6 +26,24 @@ function vowelsCounter(term, vocoidList) {
   return vocoidCounter;
 }
 
+//! ARROW FUNCTION
+// /**
+//  *todo      Funzione che data una stringa ed un array di vocali ritorna il numero di vocali presenti all'interno della stringa fornitogli
+//             definita la variabile con let perchè deve modificarsi ad ogni ciclo aumentando
+//             Utilizzo il doppio ciclo prendendo col ciclo esterno una lettera e con quello interno controllo se quella lettera coincide con una delle vocali
+//  *          @param {string} terms
+//  *          @param {string[]} vocoidList
+//  *          @returns
+//  */
+//^  const vowelsCounter = (terms, vocoidList) => {
+//^    let vocoidCounter = 0;
+//^    for (const char of terms) {
+//^      for (const charVocal of vocoidList) {
+//^        if (charVocal === char) vocoidCounter += 1;
+//^      }
+//^    }
+//^    return vocoidCounter;
+//^  };
 
 // Invoca la funzione qui e stampa il risultato in console
 const numberOfVowels = vowelsCounter(word, typeOfVowels);
