9682aa8ae40bb3c269db0bb722c521cbbd5ecf10
SirioRedGiant <Oiris1992@gmail.com>
2026-01-09 19:11:43 +0100

    eXERCISE 4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,27 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
+const word = "javascript";
+const typeOfVowels = ["a", "e", "i", "o", "u"];
 
 // Dichiara la funzione qui.
 
+//! FUNZIONE TRADIZIONALE
+
+function vowelsCounter(term, vocoidList) {
+  let vocoidCounter = 0;
+  for (const char of term) {
+    for (const charVocal of vocoidList) {
+      if (charVocal === char) {
+        vocoidCounter += 1;
+      }
+    }
+  }
+  return vocoidCounter;
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const numberOfVowels = vowelsCounter(word, typeOfVowels);
+console.log(numberOfVowels);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
