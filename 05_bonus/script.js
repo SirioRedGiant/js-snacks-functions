5baf644c4d8dac355e1e03f2b24c9ae09178ddea
SirioRedGiant <Oiris1992@gmail.com>
2026-01-09 23:27:46 +0100

    Traditional function with some problem

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -6,13 +6,30 @@ e buonasera se è sera (oltre le 17)
 */
 
 const name = 'Mario';
+const whatTimeIsIt = prompt("What time is it? (e.g. 12.35 p.m.)", "13.05 p.m." );
 
 
 // Dichiara la funzione qui.
 
+//! TRADITIONAL FUNCTION
+
+function GreetingBasedOnTime(user, time) {
+    const whatHour = parseInt(time)
+    if (whatHour <= 13.00) {
+        return `good morning ${user}`
+    }
+    else if (whatHour >= 13.00 && whatHour <= 17.00) {
+        return `good afternoon ${user}`
+    }
+    else {
+        return `good evening ${user}`
+    }
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const GreetOntime = GreetingBasedOnTime(name, whatTimeIsIt)
+console.log(GreetOntime)
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
