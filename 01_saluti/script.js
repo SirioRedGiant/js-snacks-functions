1d61bdd2a170f7be25e70c1f64332616ceea91dd
SirioRedGiant <Oiris1992@gmail.com>
2026-01-09 15:20:14 +0100

    First exercise => Funzione di saluto in chiave tradizionale e moderna

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -1,13 +1,26 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const userName = 'Mario';
-
+const userName = "Mario";
 
 // Dichiara la funzione qui.
+//! FUNZIONE TRADIZIONALE
+/**
+ * Funzione che saluta il nuovo utente
+ * @param {string} name
+ * @returns {string}
+ */
+function greeter(name) {
+  return "Ciao " + name + " è un piacere averti qui!";
+}
 
+/**
+ *! FUNZIONE MODERNA
+ * todo l'uso di ${name} sostituisce la concatenzione classica il RETURN è implicito nelle arrow function con una sola riga di codice, si possono omettere anche le parentesi graffe 
+
+ *? const greeter = (name) => return `Ciao ${name} è un piacere averti qui!`
+*/
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
-
+console.log(greeter(name));
 //Risultato atteso se si passa 'Mario': // ciao Mario
+console.log(greeter(userName));
